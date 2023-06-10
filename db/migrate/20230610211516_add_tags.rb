8d65d6596620f65cba3bf6b85866a3783d82c0d5
Taspee <tfaspee@miuandes.cl>
2023-06-10 17:23:42 -0400

    sorry

--- db/migrate/20230610211516_add_tags.rb
+++ db/migrate/20230610211516_add_tags.rb
@@ -3,5 +3,6 @@ class AddTags < ActiveRecord::Migration[7.0]
     create_table :tags do |t|
       t.references :tweet, foreign_key: true
       t.references :hashtag, foreign_key: true
+    end
   end
 end
